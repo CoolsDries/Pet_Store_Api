0814f87ab8635583c1bb2f718f19661c300b5da2
Dries Cools <dries.cools@hotmail.com>
2024-09-30 17:16:03 +0200

    Created StoreRepository interface and (scaffolding) implemented interface

---
+++ Data/Repositories/StoreRepository.cs
@@ -0,0 +1,49 @@
+﻿using Pet_Store_Api.Models;
+
+namespace Pet_Store_Api.Data.Repositories
+{
+    public class StoreRepository : IStoreRepository, IDisposable
+    {
+        private readonly PetStoreContext _context;
+
+        public StoreRepository(PetStoreContext context) 
+        {
+            _context = context;
+        }
+
+        public void DeleteStore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Store GetStoreByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Store> GetStores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertStore(Store store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStore(Store store)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Pet_Store_Api.Models
+{
+    public interface IStoreRepository:IDisposable
+    {
+        Store GetStoreByID(int id);
+        IEnumerable<Store> GetStores();
+        void InsertStore(Store store);
+        void DeleteStore(int id);
+        void UpdateStore(Store store);
+        void Save();
+    }
+}
