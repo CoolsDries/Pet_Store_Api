9a1bf2e2861dd36ddf38d4816810c44d3daf4c80
Dries Cools <dries.cools@hotmail.com>
2024-09-30 16:32:01 +0200

    Created PetStoreContext and Scaffolded StoresController

---
+++ Data/PetStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pet_Store_Api.Models;
+
+namespace Pet_Store_Api.Data
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetStoreContext(DbContextOptions<PetStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Store> Stores { get; set; } = null!;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Pet_Store_Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PetStoreContext>(opt =>
+    opt.UseInMemoryDatabase("PetStore"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
